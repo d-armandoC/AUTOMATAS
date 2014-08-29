0a619a9872445640561e0672e6596f0e85c093d1
silvia <silvy_revelde@hotmail.com>
2014-08-29 09:38:55 -0500

    Validacion y corrrecciones reportes  exceso_velocidad  by silvia

--- index_admin.php
+++ index_admin.php
@@ -87,6 +87,7 @@
         <script type="text/javascript" src="js/interface/report/ventanaCmdHist.js"></script>
         <script type="text/javascript" src="js/interface/report/ventanaReporteParadas.js"></script>
         <script type="text/javascript" src="js/interface/report/ventanaReporteConsumoCombustible.js"></script>
+        <script type="text/javascript" src="js/interface/report/ventanaEncendido.js"></script>
       
 
         <script type="text/javascript" src="js/interface/adminGeo/ventanaVerGeo.js"></script>
